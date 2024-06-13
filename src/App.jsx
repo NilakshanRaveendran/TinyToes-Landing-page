c2da41db1cc354b7688a2d550ae2f74c9e807256
Nilakshan Raveendran <nilaksh2001@gmail.com>
2024-06-13 18:43:23 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Navbar from './Component/Navbar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Component/Home';
-import About from './Component/About';
-import Contact from './Component/Contact';
 
 
 const App = () => {
@@ -14,8 +12,6 @@ const App = () => {
           <div className="content">
             <Routes>
               <Route path = "/" element = {<Home/>}/>
-              <Route path = "/about" element = {<About />}/>
-              <Route path = "/contact" element = {<Contact />} />
             </Routes>
           </div>
         </div>
