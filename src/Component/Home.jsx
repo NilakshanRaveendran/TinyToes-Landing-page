0cd97f42390d61d800f6f53143c2bd3be956b516
Nilakshan Raveendran <nilaksh2001@gmail.com>
2024-06-18 14:45:54 +0530

    add

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -7,16 +7,17 @@ import Real from './Real'
 import Update from './Update'
 import Faq from './Faqs'
 
-
 const Home = () => {
   return (
     <>
     <div className="home">
-        <div className="home-content">
+      
+      <div className="home-content">
           <h2>Tiny Toes</h2>
           <p>An online platform facilitating childcare and nursing services, connecting families with qualified caregivers and healthcare professionals. </p>
         </div>
-          <img src={image1} alt="image1" className='image1'/>
+        <img src={image1} alt="image1" className='image1'/>
+        
     </div>
 
     <div className="about-content">
